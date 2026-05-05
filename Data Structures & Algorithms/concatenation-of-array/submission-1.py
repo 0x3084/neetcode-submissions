c604d8d7403dc0f5e4b769d67996e50c757dfc99
0x3084 <109823098+0x3084@users.noreply.github.com>
2026-05-05 04:32:30 +0200

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+
+    	ans =  [None] * len(nums) * 2
+
+    	for m in range(len(nums)):
+    		ans[m] = nums[m]
+    		ans[m + len(nums)] =nums[m]
+    	return ans
+
+m = Solution()
+
